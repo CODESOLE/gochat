bd4d992f857b1de786a5d08a1ad569548406649b
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2023-12-30 01:56:56 +0300

    server bug fixing

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -27,49 +27,36 @@ func handle_client(conn net.Conn, msg_ch chan core.Payload) {
 		pl.SendTime = time.Now().String()
 		if err != nil {
 			log.Printf("Client(%s) disconnected!\n", conn.RemoteAddr().String())
+			conn.Close()
 			mu.Lock()
 			clients = slices.DeleteFunc(clients, func(c net.Conn) bool {
-				return c.RemoteAddr().String() == conn.RemoteAddr().Network()
+				return c.RemoteAddr().String() == conn.RemoteAddr().String()
 			})
 			mu.Unlock()
-			conn.Close()
 			return
 		}
-		fmt.Printf("Client(%s) send: %s", conn.RemoteAddr().String(), string(pl.Msg[:n]))
+		log.Printf("Client(%s) send: %s", conn.RemoteAddr().String(), string(pl.Msg[:n]))
 		msg_ch <- pl
 	}
 }
 
-func filter[T any](ss []T, test func(T) bool) (ret []T) {
-	for _, s := range ss {
-		if test(s) {
-			ret = append(ret, s)
-		}
-	}
-	return
-}
-
 func server(msg_ch chan core.Payload) {
 	for s := range msg_ch {
-		others := filter[net.Conn](clients, func(c net.Conn) bool {
-			return c.RemoteAddr().String() != s.IpAddr
-		})
-		for i := range others {
-			fmt.Println(others[i].RemoteAddr().String())
-			_, err := others[i].Write(s.Msg)
+		for _, c := range clients {
+			_, err := c.Write(s.Msg)
 			if err != nil {
 				s.Conn.Close()
 				log.Printf("Cannot send message to %v\n", s.IpAddr)
 				continue
 			}
-			log.Printf("Message sent from %v to %v", s.IpAddr, others[i].RemoteAddr().String())
+			log.Printf("Message sent from %v to %v", s.IpAddr, c.RemoteAddr().String())
 		}
 	}
 }
 
 func main() {
 	if len(os.Args) < 2 {
-		log.Fatalln("Absent argument! You must specify the PORT to be listened in.")
+		log.Fatalln("Missing argument! You must specify the PORT to be listened in.")
 	}
 	port := os.Args[1]
 
