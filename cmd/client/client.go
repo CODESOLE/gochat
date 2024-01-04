8582a688c141d39852bb7c0c303218df65ada42a
CODESOLE <22984206+CODESOLE@users.noreply.github.com>
2024-01-04 20:29:31 +0300

    library changes and some changes

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -5,29 +5,31 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 	"time"
 
-	"github.com/CODESOLE/gochat/cmd/client/tui"
-	"github.com/CODESOLE/gochat/internal/core"
-	ui "github.com/gizak/termui/v3"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
 )
 
-func handle_incoming_msg(conn *net.TCPConn) {
+// var boxes []tview.Box
+
+func handle_incoming_msg(conn *net.TCPConn, flex *tview.Flex, app *tview.Application) {
 	reply := make([]byte, 255)
 
 	for {
 		n, err := conn.Read(reply)
-    core.PushStack(reply[:n], conn.RemoteAddr().String(), time.Now().String())
 		if err != nil {
 			fmt.Println("Read from server failed:", err.Error())
 			conn.Close()
 			os.Exit(1)
 		}
+		tv := tview.NewTextView().SetChangedFunc(func() { app.Draw() })
+    trimmed_str := strings.TrimSpace(string(reply[:n]))
+		fmt.Fprintf(tv, "%s", trimmed_str)
+		tv.SetBorder(true).SetTitle(time.Now().String())
+		flex.AddItem(tv, 0, 1, false)
 	}
-}
-
-func is_alphanum(c byte) bool {
-	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9')
 }
 
 func main() {
@@ -41,59 +43,33 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := ui.Init(); err != nil {
-		log.Fatalf("failed to initialize termui: %v", err)
-	}
-	defer ui.Close()
-
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
 		fmt.Println("Couldn' t connect to server':", err.Error())
 		os.Exit(1)
 	}
-	go handle_incoming_msg(conn)
 
-	buf := make([]byte, 255)
-	uiEvents := ui.PollEvents()
-	ticker := time.NewTicker(time.Millisecond * 100).C
-	var ui_width, ui_height int = ui.TerminalDimensions()
-	for {
-		select {
-		case e := <-uiEvents:
-			switch e.ID { // event string/identifier
-			case "q", "<C-c>": // press 'q' or 'C-c' to quit
-				return
-			case "<Resize>":
-				payload := e.Payload.(ui.Resize)
-				ui_width, ui_height = payload.Width, payload.Height
+	app := tview.NewApplication()
+	inputField := tview.NewInputField()
+	inputField.SetLabel("Enter Message: ").
+		SetPlaceholder("Your message").
+		SetFieldWidth(0).
+		SetDoneFunc(func(key tcell.Key) {
+			if key == tcell.KeyCtrlC {
+				app.Stop()
+			} else if key == tcell.KeyEnter {
+				conn.Write([]byte(inputField.GetText()))
+				inputField.SetText("")
 			}
-			switch e.Type {
-			case ui.KeyboardEvent: // handle all key presses
-				eventID := e.ID // keypress string
-				if eventID == "<Tab>" {
-					for range [4]byte{} {
-						buf = append(buf, ' ')
-					}
-				} else if eventID == "<Space>" {
-					buf = append(buf, ' ')
-				} else if eventID == "<Backspace>" || eventID == "<C-<Backspace>>" {
-          buf = buf[:len(buf) - 1]
-				}
-				if len(eventID) == 1 && is_alphanum(eventID[0]) {
-					buf = append(buf, eventID[0])
-				} else if eventID == "<Enter>" {
-					_, err = conn.Write(buf)
-					if err != nil {
-						conn.Close()
-						fmt.Println("Write to server failed: ", err.Error())
-						os.Exit(1)
-					}
-					buf = nil
-				}
-			}
-		// use Go's built-in tickers for updating and drawing data
-		case <-ticker:
-			tui.RenderPromptUI(ui_width, ui_height, buf)
-		}
+		})
+	inputField.SetBorder(true)
+
+	flex := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(inputField, 0, 1, true)
+
+	go handle_incoming_msg(conn, flex, app)
+
+	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
+		panic(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,17 @@ module github.com/CODESOLE/gochat
 
 go 1.21.5
 
-require github.com/gizak/termui/v3 v3.1.0
+require (
+	github.com/gdamore/tcell/v2 v2.6.1-0.20231203215052-2917c3801e73
+	github.com/rivo/tview v0.0.0-20240101144852-b3bd1aa5e9f2
+)
 
 require (
-	github.com/mattn/go-runewidth v0.0.2 // indirect
-	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
-	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/term v0.9.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 )
